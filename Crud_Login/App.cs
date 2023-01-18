50e74c5854df69d8faf0ea2fe4c66e27fdf9230a
Jonas Dias <zdias1413@gmail.com>
2023-01-17 21:35:02 -0300

    Primeiro commit

---
+++ Crud_Login/App.cs
@@ -0,0 +1,9 @@
+namespace Crud_Login {
+    internal static class App {
+        [STAThread]
+        static void Main() {
+            ApplicationConfiguration.Initialize();
+            Application.Run();
+        }
+    }
+}
